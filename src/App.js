cd1e11b59cffae515a65bd7eec89d350de368d31
sang huynh <sanghuynhnt95@gmail.com>
2020-01-16 18:04:24 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback} from 'react'
+import { useDropzone } from 'react-dropzone'
+
+import './App.css'
+
+function Dropzone() {
+
+  const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = error => reject(error)
+  })
+
+  const onDrop = useCallback(async acceptedFiles => {
+    let base64_string = await toBase64(acceptedFiles[0])
+    base64_string = base64_string.replace('data:image/jpeg;base64','').replace('data:image/png;base64','')
+    var buffer = Buffer.from(base64_string, 'base64')
+    const image = cv.imdecode(buffer);
+    console.log("image", image)
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Dropzone />
       </header>
     </div>
   );
